67a486cc1f0e6db9fa5b0957aaa7f0185124882c
Tom Clemmet <tomclemmet@gmail.com>
2026-04-02 17:55:37 +0100

    Handle ALDVMM failure in checks

--- script/5L/5L-aldvmm-checks.R
+++ script/5L/5L-aldvmm-checks.R
@@ -4,6 +4,7 @@ library(readr)
 library(ggplot2)
 library(aldvmm)
 library(writexl)
+library(tictoc)
 
 # Load data
 hse <- read_csv("Data/hse-5L.csv", show_col_types = FALSE)
@@ -25,11 +26,15 @@ coefs <- list(
   )
 
 
-# ALDVMM test function, returning table of AICs
+# ALDVMM test function, returning table of loglike
 aldvmm_test <- function(sex, n_comp) {
-  aics <- data.frame(matrix(ncol = length(inits) + 1, nrow = length(optims)))
-  colnames(aics) <- c("method", inits)
-  aics$method <- optims
+  message(paste(sex, n_comp, "component:"))
+  loglike <- data.frame(matrix(ncol = length(inits) + 1, nrow = length(optims)))
+  times <- data.frame(matrix(ncol = length(inits) + 1, nrow = length(optims)))
+  colnames(loglike) <- c("method", inits)
+  loglike$method <- optims
+  colnames(times) <- c("method", inits)
+  times$method <- optims
   for (i in 1:length(optims)) {
     for (j in 1:length(inits)) {
       tic()
@@ -41,7 +46,7 @@ aldvmm_test <- function(sex, n_comp) {
           ncmp = n_comp,
           optim.method = optims[i],
           init.est = coefs[[n_comp]]
-        ) |> suppressMessages()
+        ) |> suppressMessages() |> suppressWarnings()
       } else {
         model <- aldvmm(
           index ~ I(age/10) + I((age/10)^2) | I(age/10),
@@ -50,21 +55,28 @@ aldvmm_test <- function(sex, n_comp) {
           ncmp = n_comp,
           optim.method = optims[i],
           init.method = inits[j]
-          ) |> suppressMessages()
+          ) |> suppressMessages() |> suppressWarnings()
       }
-      aics[i, j+1] <- model$gof$aic[[1]]
-      message <- paste(optims[i], inits[j], toc(quiet = TRUE)$callback_msg)
+      if (anyNA(model$cov) | any(diag(model$cov) <= 0) | any(is.nan(model$pred$yhat))) {
+        loglike[i, j+1] <- NA
+      } else {
+        loglike[i, j+1] <- model$gof$ll[[1]]
+      }
+      t <- toc(quiet = TRUE)$callback_msg
+      times[i, j+1] <- as.numeric(sub("^([0-9.]+).*", "\\1", t))
+      message <- paste(optims[i], inits[j], t)
       message(message)
     }
   }
-  print(aics)
-  export <- list()
-  export[[paste(sex, n_comp, "component")]] <- aics
-  write_xlsx(export, "output-5L/2-data/aldvmm-checks.xlsx")
+  message("Log likelihood:")
+  print(loglike)
+  message("Runtimes:")
+  print(times)
 }
 
-for (sex in 1:2) {
-  for (n in 1:3) {
-    aldvmm_test(sex, n)
+
+for (i in 1:2) {
+  for (j in 1:3) {
+    aldvmm_test(sex[i], j)
   }
 }
